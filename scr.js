547442ef89ed77e5a1052fae29dea9cf87369b2a
kangwangoioi <kangwang0101@gmail.com>
2023-12-25 12:11:39 +0700

    Update scr.js

--- scr.js
+++ scr.js
@@ -1,12 +1,11 @@
-console.log(ten);
-console.log(pas)
+
 
 var click = document.getElementById("xn")
 click.addEventListener("click",getvalue)
 
 function getvalue(event){
     event.preventDefault()
-    var tuoi=document.getElementById("tuoi").value
+    var ten=document.getElementById("ten").value
     var pas=document.getElementById("pas").value    
     console.log(ten)
 console.log(pas)
@@ -15,7 +14,7 @@ console.log(pas)
 
 //cach luu data
 localStorage.setItem("data", JSON.stringify(ten))
-
+localStorage.setItem("data", JSON.stringify(pas))
 //cach lay data ve may
 // console.log(localStorage.getItem("data"))
 // document.write(localStorage.getItem("data"))
