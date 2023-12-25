e7139abc947bb017e454efbd4fe2aaeae2a73e4d
kangwangoioi <kangwang0101@gmail.com>
2023-12-25 12:36:36 +0700

    real
    real

---
+++ bai2.js
@@ -0,0 +1,20 @@
+//bai 2
+
+setTimeout(function(){
+    document.writeln("Welcome to the internet"+"<br>");
+}, 3000);
+setTimeout(function(){
+    document.writeln("  Come and take a seat"+"<br>");
+}, 4000);
+setTimeout(function(){
+    document.writeln("      Would you like to see the news or any famous women's feet?"+"<br>");
+}, 5000);
+setTimeout(function(){
+    document.writeln("          There's no need to panic"+"<br>");
+}, 6000);
+setTimeout(function(){
+    document.writeln("              This isn't a test, haha"+"<br>");
+}, 7000);
+setTimeout(function(){
+    document.writeln("                  Just nod or shake your head and we'll do the rest"+"<br>");
+},8000);
--- scr.js
+++ scr.js
@@ -1,22 +1,18 @@
 
 
-var click = document.getElementById("xn")
-click.addEventListener("click",getvalue)
+// var click = document.getElementById("xn")
+// click.addEventListener("click",getvalue)
 
-function getvalue(event){
-    event.preventDefault()
-    var ten=document.getElementById("ten").value
-    var pas=document.getElementById("pas").value    
-    console.log(ten)
-console.log(pas)
-}
+// function getvalue(event){
+//     event.preventDefault()
+//     var ten=document.getElementById("ten").value
+//     var pas=document.getElementById("pas").value    
+//     console.log(ten)
+// console.log(pas)
+// }
 
 
-//cach luu data
-localStorage.setItem("data", JSON.stringify(ten))
-localStorage.setItem("data", JSON.stringify(pas))
-//cach lay data ve may
-// console.log(localStorage.getItem("data"))
-// document.write(localStorage.getItem("data"))
-
+// //cach luu data
+// localStorage.setItem("data", JSON.stringify(ten))
+// localStorage.setItem("data", JSON.stringify(pas))
 
