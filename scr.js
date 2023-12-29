d64a5239d0dbc960bcdfd3e56925f50f008f0f56
kangwangoioi <kangwang0101@gmail.com>
2023-12-29 11:35:09 +0700

    Update scr.js

--- scr.js
+++ scr.js
@@ -1,18 +1,16 @@
 
 
-// var click = document.getElementById("xn")
-// click.addEventListener("click",getvalue)
+var click = document.getElementById("xn")
+click.addEventListener("click",getvalue)
 
-// function getvalue(event){
-//     event.preventDefault()
-//     var ten=document.getElementById("ten").value
-//     var pas=document.getElementById("pas").value    
-//     console.log(ten)
-// console.log(pas)
-// }
+function getvalue(event){
+    event.preventDefault()
+    var ten=document.getElementById("ten").value
+    var pas=document.getElementById("pas").value    
+}
 
 
-// //cach luu data
-// localStorage.setItem("data", JSON.stringify(ten))
-// localStorage.setItem("data", JSON.stringify(pas))
+localStorage.setItem("data", JSON.stringify(ten))
+localStorage.setItem("data", JSON.stringify(pas))
+console.log(localStorage.getItem("data"))
 
